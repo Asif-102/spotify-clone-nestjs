2d7102c534705419a5fa5161370b9e1f5e73ce30
Asif-102 <asif1zero2@gmail.com>
2025-03-22 14:34:00 +0600

    handle error

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -8,11 +8,15 @@ export class SongsService {
   private readonly songs = [];
 
   create(song) {
+    // Save the song in the database
     this.songs.push(song);
     return this.songs;
   }
 
   findAll() {
+    // fetch the songs from the db
+    // Errors comes while fetching the data from DB
+    throw new Error('Error in Db while fetching record');
     return this.songs;
   }
 }
