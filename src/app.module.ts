4b83e32c0fc11da318a4d4b87024a70155f42f3a
Asif-102 <asif1zero2@gmail.com>
2025-03-22 14:20:34 +0600

    apply middleware into songs route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 import { SongsModule } from './songs/songs.module';
 
 @Module({
@@ -8,4 +9,8 @@ import { SongsModule } from './songs/songs.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+  }
+}
