9ae30c6345b52da6ae1095679d68ce67c00d7c6d
Asif-102 <asif1zero2@gmail.com>
2025-03-22 15:32:52 +0600

    custorm providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { DevConfigService } from './common/providers/DevConfigService';
 import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/songs.module';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: DevConfigService,
+      useClass: DevConfigService,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
